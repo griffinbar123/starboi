887f5fd984edc5874d68e1ce34967f6454d8a216
Fabrice Mpozenzi <fxm20c@acu.edu>
2023-09-28 22:16:55 -0500

    Game levels for players to choose from

--- Starboi/src/main/java/sst/App.java
+++ Starboi/src/main/java/sst/App.java
@@ -1,13 +1,102 @@
 package sst;
+import java.util.Scanner;
+public class App {
+    private Scanner scanner;
+    public App() {
+        scanner = new Scanner(System.in);
+    }
+    public void gameLevel(String levelChoice) {
+        //System.out.println("Here is the player choice after passed in: "+levelChoice);
+        switch (levelChoice) {
+            case "novice":
+                System.out.print("Please type in a secret password (9 characters maximum)- ");
+                System.out.println();
+                break;
+            case "fair":
+                System.out.print("Please type in a secret password (9 characters maximum)- ");
+                System.out.println();
+                break;
+            case "good":
+                System.out.print("Please type in a secret password (9 characters maximum)- ");
+                System.out.println();
+                break;
+            case "expert":
+                System.out.print("Please type in a secret password (9 characters maximum)- ");
+                System.out.println();
+                break;
+            case "emeritus":
+                System.out.print("Please type in a secret password (9 characters maximum)- ");
+                System.out.println();
+                break;
+            default:
+                System.out.println("Invalid choice. Please choose Novice, Fair, Good, Expert, or Emeritus");
+                break;
+        }
+    }
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public void gameType(String gameChoice) {
+        switch (gameChoice) {
+            case "short":
+                System.out.print("Are you a Novice, Fair, Good, Expert, or Emeritus player? ");
+                String playerChoiceOne = scanner.nextLine().trim().toLowerCase();
+                System.out.println();
+                //System.out.println("Here is the player choice before passed in: "+playerChoice);
+                gameLevel(playerChoiceOne); 
+                break;
+            case "medium":
+                System.out.print("Are you a Novice, Fair, Good, Expert, or Emeritus player? ");
+                String playerChoiceTwo = scanner.nextLine().trim().toLowerCase();
+                System.out.println();
+                //System.out.println("Here is the player choice before passed in: "+playerChoice);
+                gameLevel(playerChoiceTwo); 
+                break;
+            case "long":
+                System.out.print("Are you a Novice, Fair, Good, Expert, or Emeritus player? ");
+                String playerChoiceThree = scanner.nextLine().trim().toLowerCase();
+                System.out.println();
+                //System.out.println("Here is the player choice before passed in: "+playerChoice);
+                gameLevel(playerChoiceThree); 
+                break;
+            default:
+                System.out.println("Invalid choice. Please choose short, medium, or long.");
+                break;
+        }
+    }
+    
+    public static void main(String[] args) {
+        App app = new App();
+
+        System.out.print("Would you like a regular, tournament, or frozen game? ");
+        String choice = app.scanner.nextLine().trim().toLowerCase();
+        System.out.println();
+      
+        switch (choice) {
+            case "regular":
+                System.out.print("Would you like a short, medium, or long game? ");
+                String regularChoice = app.scanner.nextLine().trim().toLowerCase();
+                System.out.println();
+                app.gameType(regularChoice);
+                // Add code for a regular game here
+                break;
+            case "tournament":
+                System.out.print("Type in a tournament number- ");
+                String tournNumber = app.scanner.nextLine();//indicate what happens for each number typed in
+                System.out.println();
+                System.out.print("Would you like a short, medium, or long game? ");
+                String tournChoice = app.scanner.nextLine().trim().toLowerCase();
+                System.out.println();
+                app.gameType(tournChoice);
+                break;
+            //implement this later
+            case "frozen":
+                System.out.println("You chose a frozen game.");
+                // Add code for a frozen game here
+                break;
+            default:
+                System.out.println("Invalid choice. Please choose regular, tournament, or frozen.");
+                break;
+        }
+
+        app.scanner.close();
     }
 }
