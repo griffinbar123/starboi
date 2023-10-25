ea4773fdb728aa86df4fd8ca4f6e8aeb7d5afcf1
Matthias Schrock <mxs21j@acu.edu>
2023-10-25 14:20:45 -0500

    Updates to Damages

--- src/main/java/Model/Enterprise.java
+++ src/main/java/Model/Enterprise.java
@@ -7,21 +7,47 @@ import lombok.NonNull;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Enterprise extends Entity {
-    private boolean srSensors = true;
-    private boolean lrSensors = true;
-    private boolean phasers = true;
-    private boolean photonTubes = true;
-    private boolean lifeSupportStutus = true;
-    private boolean warpEngines = true;
-    private boolean impulseEngines = true;
-    private boolean shields = true;
-    private boolean subspaceRadio = true;
-    private boolean shuttleCraft = true;
-    private boolean computer = true;
-    private boolean trasnporter = true;
-    private boolean shieldControl = true;
-    private boolean dsProbe = true;
-    private boolean cloakingDevice = true;
+    public enum Devices {
+        SR_SENSORS(1, "S. R. Sensors", 0.0),
+        LR_SENSORS(2, "L. R. Sensors", 0.0),
+        PHASERS(3, "Phasers", 0.0),
+        PHOTON_TUBES(4, "ProtonTubes", 0.0),
+        LIFE_SUPPORT(5, "Life Support", 0.0),
+        WARP_ENGINES(6, "Warp Engines", 0.0),
+        IMPULSE_ENGINES(7, "Impulse Engines", 0.0),
+        SHIELDS(8, "Shields", 0.0),
+        SUBSPACE_RADIO(9, "Subspace Radio", 0.0),
+        SHUTTLE_CRAFT(10, "Shuttle Craft", 0.0),
+        COMPUTER(11, "Computer", 0.0),
+        TRANSPORTER(12, "Transporter", 0.0),
+        SHIELD_CONTROL(13, "Shield Control", 0.0),
+        DEATHRAY(14, "Death Ray", 0.0),
+        DS_PROBE(15, "D. S. Probe", 0.0),
+        CLOAKING_DEVICE(16, "Cloaking Device", 0.0);
+
+        private final int deviceNum;
+        private final String deviceName;
+        private final double damage;
+
+        private Devices(int deviceNum, String deviceName, double damage) {
+            this.deviceNum = deviceNum;
+            this.deviceName = deviceName;
+            this.damage = damage;
+        }
+
+        public int getDeviceNum() {
+            return deviceNum;
+        }
+
+        public String getDeviceName() {
+            return deviceName;
+        }
+
+        public double getDamage() {
+            return damage;
+        }
+    }
+    private Devices devices;
 
     private String condition;
 
--- src/main/java/sst/Damages.java
+++ src/main/java/sst/Damages.java
@@ -1,7 +1,10 @@
 package sst;
 
-import Model.Enterprise;
+import java.util.HashMap;
+import java.util.Map;
+
 import Model.Game;
+import Model.Enterprise.Devices;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
@@ -13,30 +16,63 @@ import lombok.RequiredArgsConstructor;
 public class Damages {
     @NonNull
     private Game game;
+    private final double docFac = 0.25;
 
     /**
      * DAMAGES command implementation
      * @author Fabrice Mpozenzi
      */
     public void ExecDAMAGES() {
-        Enterprise enterprise = this.game.getEnterprise();
+        Map<Devices, Double> repairTimes = calcDamages();
+        Devices devices = game.getEnterprise().getDevices();
+        // game.con.printf("Device       -REPAIR TIMES-\n");
+        // game.con.printf("             IN FLIGHT  DOCKED\n");
+        // game.con.printf("     S. R. Sensors   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("     L. R. Sensors   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("           Phasers   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("      Photon Tubes   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("      Life Support   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("      Warp Engines   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("   Impulse Engines   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("           Shields   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("    Subspace Radio   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("     Shuttle Craft   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("          Computer   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("       Transporter   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("    Shield Control   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("       D. S. Probe   %.2f    %.2f\n", 0.0, 0.0);
+        // game.con.printf("   Cloaking Device   %.2f    %.2f\n", 0.0, 0.0);
+
         game.con.printf("Device       -REPAIR TIMES-\n");
         game.con.printf("             IN FLIGHT  DOCKED\n");
-        game.con.printf("     s. R. Sensors   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("     L. R. Sensors   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("           Phasers   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("      Photon Tubes   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("      Life Support   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("      Warp Engines   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("   Impulse Engines   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("           Shields   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("    Subspace Radio   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("     Shuttle Craft   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("          Computer   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("       Transporter   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("    Shield Control   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("       D. S. Probe   %.2f    %.2f\n", 0.0, 0.0);
-        game.con.printf("   Cloaking Device   %.2f    %.2f\n", 0.0, 0.0);
+        for (Devices device : Devices.values()) {
+            game.con.printf("  %16s %.2f    %.2f\n", device.getDeviceName(), repairTimes.get(device), repairTimes.get(device) * docFac);
+        }
     }
 
+    private Map<Devices, Double> calcDamages() {
+        Map<Devices, Double> repairTimes = new HashMap<>();
+        Devices enterpriseDevices = game.getEnterprise().getDevices();
+
+        // calculate in-flight repair times for all devices
+        // TODO use the enterprise's devices rather than the device enum (which will always have damage of 0.0)
+        for (Devices device : Devices.values()) {
+            if (device.getDamage() > 0.0) {
+                if (device == Devices.DEATHRAY) {
+                    repairTimes.put(device, device.getDamage() + 0.005);
+                } else {
+                    repairTimes.put(device, device.getDamage() + 0.05);
+                }
+            }
+        }
+        return repairTimes;
+    }
+
+    /**
+     * Public facing method for calculating and updating damages throughout
+     * the game as damage is incurred by any entitity
+     */
+    public void assessDamages() {
+        
+    }
 }
