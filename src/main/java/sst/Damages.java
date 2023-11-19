170d40f0aa6c94de94636f2b7606255d9dc5a15a
Matthias Schrock <mxs21j@acu.edu>
2023-11-19 08:21:11 -0600

    Revert "updated assessDamages method."
    This reverts commit c743cb91330cf07c6adaa93ab3e1daa8030e54fc.

--- src/main/java/sst/Damages.java
+++ src/main/java/sst/Damages.java
@@ -1,30 +1,22 @@
 package sst;
 
-import static Utils.Utils.*;
-
 import java.util.HashMap;
 import java.util.Map;
 
-import Model.Device;
 import Model.Enterprise;
-import Model.Entity;
-import Model.EntityType;
 import Model.Game;
-import Model.ShieldStatus;
+import Model.Device;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import sst.Finish.GameOverReason;
 
 /**
  * implements the damages command and functionality
- * 
  * @author Fabrice Mpozenzi
  */
 @RequiredArgsConstructor
 public class Damages {
     @NonNull
     private Game game;
-    public Damages typeDamageScore;
     /**
      * Factor by which repair times are multiplied when docked
      */
@@ -32,7 +24,6 @@ public class Damages {
 
     /**
      * DAMAGES command implementation
-     * 
      * @author Fabrice Mpozenzi
      */
     public void ExecDAMAGES() {
@@ -61,7 +52,6 @@ public class Damages {
 
     /**
      * Calculates the repair times for each device
-     * 
      * @return a map of devices and their (in-flight) repair times
      * @author Matthias Schrock
      * @author Fabrice Mpozenzi
@@ -89,55 +79,14 @@ public class Damages {
      * Public facing method for calculating and updating damages throughout
      * the game as damage is incurred by any entitity
      */
-
     public void assessDamages() {
         // update devices with damage incurred
-        /*
-         * for (Device device : Device.values()) {
-         * // if should be damaged ... else ... (no damage)
-         * if (game.getEnterprise().getDeviceDamage().get(device) > 0.0) {
-         * game.getEnterprise().getDeviceDamage().put(device,
-         * game.getEnterprise().getDeviceDamage().get(device) + 0.05);
-         * }
-         * }
-         */
-        double typeDamageScore = 1.0;
-        Entity entity = game.getEntityAtPosition(game.getEnterprise().getPosition());
-        EntityType entityType = entity.getType();
-
-        switch (entityType) {
-            case ROMULAN:
-                typeDamageScore = 1.5;
-                break;
-            case COMMANDER:
-                typeDamageScore = 2.0;
-                break;
-            case SUPER_COMMANDER:
-                typeDamageScore = 2.5;
-                break;
-            case THOLIAN:
-                typeDamageScore = 0.5;
-                break;
-            default:
-                break;
-        }
-
-        for (Map.Entry<Device, Double> entry : game.getEnterprise().getDeviceDamage().entrySet()) {
-            if (entry.getKey() == Device.DEATHRAY)
-                continue;
-            if (entry.getValue() < 0)
-                continue;
-            double damage = (10.0 * typeDamageScore * randDouble(0, 1) + 1.0) * game.getDamageFactor();
-            game.getEnterprise().getDeviceDamage().put(entry.getKey(), damage);
-
-        }
-        game.getEnterprise().getSheilds().setStatus(ShieldStatus.DOWN);
-
-        if (game.getRemainingKlingonCount() > 0) {
-            // pause
-            new Damages(game).ExecDAMAGES();
-        } else {
-            new Finish(game).finish(GameOverReason.WON);
+        for (Device device : Device.values()) {
+            // if should be damaged ... else ... (no damage)
+            if (game.getEnterprise().getDeviceDamage().get(device) > 0.0) {
+                game.getEnterprise().getDeviceDamage().put(device,
+                        game.getEnterprise().getDeviceDamage().get(device) + 0.05);
+            }
         }
 
         // make sure to call print after this update damages
